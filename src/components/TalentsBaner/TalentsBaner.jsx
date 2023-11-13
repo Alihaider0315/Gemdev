b8fe72825ee4eedb8d457701ed6259b0aaedd29c
Ali Haider <ah712200@gmail.com>
2023-11-14 03:27:25 +0500

    Update TalentsBaner.jsx

--- src/components/TalentsBaner/TalentsBaner.jsx
+++ src/components/TalentsBaner/TalentsBaner.jsx
@@ -96,10 +96,10 @@ export default function TalentsBnaer() {
                     <h3 className="name_heading">{item?.name}</h3>
                    
                     {item?.gender === 'Male' && (
-                      <h3 className="name_heading">Actorsssssssss</h3>
+                      <h3 className="name_heading">Actors</h3>
                     )}
                     {item?.gender === 'Female' && (
-                      <h3 className="name_heading">Actresdasdasss</h3>
+                      <h3 className="name_heading">Actress</h3>
                     )}
                   </Link>
                 </SwiperSlide>
